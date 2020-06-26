66e00974598ce9921a3c886038efb9bd555fa7a2
tim <beatham.tim@gmail.com>
2020-06-26 17:42:08 +0100

    Enemies following the player

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #define MAX_BULLETS 10
+#define MAX_ENEMIES 20
 
 const int screenWidth = 1024;
 const int screenHeight = 768;
@@ -16,65 +17,77 @@ int main() {
 
     SetTargetFPS(60);
 
-    float xPos = screenWidth / 2;
-    float yPos = screenHeight / 2;
-
-    Texture2D main_player = LoadTexture("img/player.png");
-
-    // What part of the texture we will use to draw the player.
-    Rectangle sourceRec = { 0.0f, 0.0f, main_player.width, main_player.height};
-
-    // Define the point in the world where the texture is to be displayed.
-    // I could scale it if I wanted to.
-    Rectangle player = {xPos, yPos, main_player.width, main_player.height};    
-
-    // Origin of the texture relative to the destination rectangle.
-    // Point of the texture used as reference for rotation and scaling.
-    Vector2 origin = {main_player.width / 2, main_player.height / 2};
-
     // Array of bullets in the game.
     Bullet *bullets = instantiateBullets();
 
+    Player player = instantiatePlayer();
 
-    float rotation = 0.0F;
+    Enemy *enemies = instantiateEnemies();
+
+    spawnEnemy(enemies);
 
     // The Main Game Loop.
     while (!WindowShouldClose()) {
 
         // Work out the rotation of the player.
-        float yDifference = GetMouseY() - player.y;
-        float xDifference = GetMouseX() - player.x;
+        float yDifference = GetMouseY() - player.boundingBox.y;
+        float xDifference = GetMouseX() - player.boundingBox.x;
 
-        rotation = (atan2(-xDifference, yDifference) / (2 * PI)) * 360;       
+        player.rotation = (atan2(-xDifference, yDifference) / (2 * PI)) * 360; 
 
-        if (IsKeyDown(KEY_D)) player.x += 2.0f;
-        if (IsKeyDown(KEY_A)) player.x -= 2.0f;
-        if (IsKeyDown(KEY_W)) player.y -= 2.0f;
-        if (IsKeyDown(KEY_S)) player.y += 2.0f;
+        movePlayer(&player);             
 
         if (IsKeyReleased(KEY_SPACE)) {
             // Convert the rotation to radians.
-            fire(bullets, player.x, player.y, (rotation / 360) * 2 * PI);
+            fire(bullets, player.boundingBox.x, player.boundingBox.y, (player.rotation / 360) * 2 * PI);
         }
 
         updateBullets(bullets);
+        updateEnemies(enemies, player);
+        killEnemies(enemies, bullets);
+
+        if (isPlayerDead(enemies, player))
+            player.isAlive = false;
 
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
+            if (player.isAlive)
+                DrawRectanglePro(player.boundingBox, player.origin, player.rotation, RED);
+            
+            
+            drawBullets(bullets); 
+            drawEnemies(enemies, player);
 
-            DrawTexturePro(main_player, sourceRec, player, origin, rotation, WHITE);
-
-            drawBullets(bullets);
-
-        
 
         EndDrawing();
+
+       
     }
 
     CloseWindow();
     return 0;
+}
+
+Player instantiatePlayer() {
+
+    Player player;
+
+    player.size = 50;
+    player.boundingBox = (Rectangle) {screenWidth / 2, screenHeight / 2, player.size, player.size};
+    player.rotation = 0;
+    player.origin = (Vector2) {player.size/2, player.size/2};
+    player.isAlive = true;
+
+    return player;
+}
+
+void movePlayer(Player* player) {
+    if (IsKeyDown(KEY_D)) player -> boundingBox.x += 2.0f;
+    if (IsKeyDown(KEY_A)) player -> boundingBox.x -= 2.0f;
+    if (IsKeyDown(KEY_W)) player -> boundingBox.y -= 2.0f;
+    if (IsKeyDown(KEY_S)) player -> boundingBox.y += 2.0f;
 }
 
 
@@ -83,14 +96,15 @@ Bullet *instantiateBullets() {
     Bullet *bullets = (Bullet*) malloc(sizeof(Bullet) * MAX_BULLETS);
 
     for (int i = 0; i < MAX_BULLETS; i++) {
+        float size = 10;
         Bullet bullet;
-        Vector2 position = {0, 0};
-        float size = 5;
-        bullet.position = position;
-        bullet.velocity = 20;
+        Rectangle boundingBox = {0, 0, size, size};
+        bullet.boundingBox = boundingBox;
+        bullet.velocity = 5;
         bullet.isActive = false;
         bullet.size = size;
         bullet.angle = 0;
+        bullet.origin = (Vector2) {bullet.size / 2, bullet.size / 2};
         bullets[i] = bullet;
     }
 
@@ -113,8 +127,8 @@ void fire(Bullet *bullets, float playerPosX, float playerPosY, float angle) {
 
     if (bullet != NULL) {
         bullet -> isActive = true;
-        bullet -> position.x = playerPosX;
-        bullet -> position.y = playerPosY;
+        bullet -> boundingBox.x = playerPosX;
+        bullet -> boundingBox.y = playerPosY;
         bullet -> angle = angle;
     }
 }
@@ -124,7 +138,7 @@ void drawBullets(Bullet *bullets) {
 
     for (int i = 0; i < MAX_BULLETS; i++) {
         if (bullets[i].isActive) {
-            DrawCircleV(bullets[i].position, bullets[i].size, RED);
+            DrawRectanglePro(bullets[i].boundingBox, bullets[i].origin, 0, RED);
         }
     }
 }
@@ -134,15 +148,100 @@ void updateBullets(Bullet *bullets) {
     for (int i = 0; i < MAX_BULLETS; i++) {
         if (bullets[i].isActive) {
             // Update the position of the bullet.
-            bullets[i].position.x += bullets[i].velocity * -sinf(bullets[i].angle);
-            bullets[i].position.y += bullets[i].velocity * cosf(bullets[i].angle);
+            bullets[i].boundingBox.x += bullets[i].velocity * -sinf(bullets[i].angle);
+            bullets[i].boundingBox.y += bullets[i].velocity * cosf(bullets[i].angle);
 
             // Check if it has gone out of the screen.
 
-            if (bullets[i].position.x > screenWidth || bullets[i].position.x < 0 
-                                    || bullets[i].position.y > screenHeight || bullets[i].position.y < 0) {
+            if (bullets[i].boundingBox.x > screenWidth || bullets[i].boundingBox.x < 0 
+                                    || bullets[i].boundingBox.y > screenHeight || bullets[i].boundingBox.y < 0) {
                 bullets[i].isActive = false;
             } 
         }
     }
 }
+
+Enemy *instantiateEnemies() {
+    Enemy *enemies = (Enemy*) malloc(sizeof(Enemy) * MAX_ENEMIES);
+
+    // Populating each enemy in the enemies list.
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        Enemy enemy;
+
+        enemy.size = 20;
+        enemy.boundingBox = (Rectangle) {0, 0, enemy.size, enemy.size};
+        enemy.origin = (Vector2) {enemy.size / 2, enemy.size / 2};
+        enemy.rotation = 0;
+        enemy.isAlive = false;
+        enemy.velocity = 2;
+
+        enemies[i] = enemy;
+    }
+
+    return enemies;
+}
+
+void spawnEnemy(Enemy *enemies) {
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        if (!enemies[i].isAlive) {
+            enemies[i].isAlive = true;
+            enemies[i].boundingBox.x = 100;
+            enemies[i].boundingBox.y = 100;
+            break;
+        }
+    }
+}
+
+void updateEnemies(Enemy *enemies, Player player) {
+    
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+         // Work out the rotation of the player.
+        float yDifference = player.boundingBox.y - enemies[i].boundingBox.y;
+        float xDifference = player.boundingBox.x - enemies[i].boundingBox.x;
+
+        float rotation = atan2(-xDifference, yDifference);
+            
+        enemies[i].rotation = (rotation / (2 * PI)) * 360; 
+
+        enemies[i].boundingBox.x += enemies[i].velocity * -sinf(rotation);
+        enemies[i].boundingBox.y += enemies[i].velocity * cosf(rotation);
+
+
+    }
+
+}
+
+void drawEnemies(Enemy *enemies, Player player) {
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        if (enemies[i].isAlive) {
+            DrawRectanglePro(enemies[i].boundingBox, enemies[i].origin, enemies[i].rotation, BLUE);
+        }
+    }
+
+}
+
+bool isPlayerDead(Enemy* enemies, Player player) {
+    // There is a more efficient way but this game is simple.
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        if (enemies[i].isAlive) {
+            if (CheckCollisionRecs(player.boundingBox, enemies[i].boundingBox))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+void killEnemies(Enemy* enemies, Bullet* bullets) {
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        for (int j = 0; j < MAX_ENEMIES; j++) {
+            if (enemies[j].isAlive && bullets[i].isActive) {
+                if (CheckCollisionRecs(bullets[i].boundingBox, enemies[j].boundingBox)) {
+                    enemies[j].isAlive = false;
+                    bullets[i].isActive = false;
+                }
+            }
+        }
+    } 
+}
+
--- main.h
+++ main.h
@@ -4,26 +4,43 @@
 #include "raylib.h"
 
 typedef struct {
-    Vector2 position;
     float velocity;
     bool isActive;
     float angle;
     float size;
+    Vector2 origin;
+    Rectangle boundingBox;
 } Bullet;
 
 typedef struct {
-    Vector2 position;
-    Vector2 size;
-    float velocity;
-    Texture2D player;
+    Rectangle boundingBox;
+    int size;
+    float rotation;
+    Vector2 origin;
+    bool isAlive;
 } Player;
+
+
+typedef struct {    
+    Rectangle boundingBox;
+    int size;
+    float rotation;
+    bool isAlive;
+    Vector2 origin;
+    float velocity;
+} Enemy;
 
 
 Bullet *instantiateBullets();
 Player instantiatePlayer();
+Enemy *instantiateEnemies();
 void fire(Bullet *bullets, float playerPosX, float playerPosY, float angle);
 void drawBullets(Bullet *bullets);
 void updateBullets(Bullet *bullets);
-
-
+void movePlayer(Player* player);
+void drawEnemies(Enemy* enemies, Player player);
+void spawnEnemy(Enemy* enemies);
+bool isPlayerDead(Enemy* enemies, Player player);
+void killEnemies(Enemy* enemies, Bullet* bullets);
+void updateEnemies(Enemy* enemies, Player player);
 #endif
